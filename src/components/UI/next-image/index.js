a37a15b3cd5dab9c1465412e0dbf0fb938d523fa
Vahan Baghdasaryan <vahan.baghdasaryan.99@gmail.com>
2023-11-09 13:05:50 +0400

    Update NextImage

--- src/components/UI/next-image/index.js
+++ src/components/UI/next-image/index.js
@@ -2,12 +2,10 @@ import React, {useState, useEffect} from 'react';
 import Image from 'next/image';
 import classNames from 'classnames';
 import fallbackImage from '@/assets/images/icons/test-img.svg';
-import PosterLoader from '@/assets/images/ezgif-5-3c8de345c7.gif';
 import styles from './index.module.scss';
 
 const Index = props => {
     const {src, fallBackSrc = fallbackImage, wrapperClass, ...rest} = props;
-    const [loading, setLoading] = useState(true);
     const [imgSrc, setImgSrc] = useState(src);
     const [isError, setError] = useState(false);
 
@@ -25,11 +23,10 @@ const Index = props => {
             ])
         }>
             <Image
-                src={loading ? PosterLoader : src ? imgSrc : fallBackSrc}
+                src={src ? imgSrc : fallBackSrc}
                 width={props.width || 300}
                 height={props.height || 450}
                 quality={80}
-                onLoad={() => setLoading(false)}
                 onError={() => {
                     setImgSrc(fallBackSrc);
                     setError(true);
